b77c98376d6bb3c019a2d527386ca768b653be99
Mehmet <mayselaksoy@gmail.com>
2020-08-11 15:28:55 -0400

    last commit from local master branc

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -6,7 +6,9 @@ public class FirstClass {
 		System.out.println("our first commit from local repo");
 		System.out.println("our third commit from local repo ");
 		System.out.println("now i am on my mehmet branch");
-
+        System.out.println("i wan to have conflictt");
+        
+        System.out.println("i want to have second conflict");
 	}
 
 }
