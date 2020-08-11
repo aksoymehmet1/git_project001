a119f61dc21dd4a23da062105262635768d61eb9
aksoymehmet1 <69370437+aksoymehmet1@users.noreply.github.com>
2020-08-11 17:34:55 -0400

    Update Mehmet1.java
    this is the third commit from remote master branch

--- src/first_package/Mehmet1.java
+++ src/first_package/Mehmet1.java
@@ -5,8 +5,8 @@ public class Mehmet1 {
 	public static void main(String[] args) {
 	System.out.println("this is my commit from mehmet1 branch");
 	System.out.println("this is the first commit from remote master  branch");
-	System.out.println("this is the seconde commit from remote master  branch");
-
+	System.out.println("this is the second commit from remote master  branch");
+	System.out.println("this is the third commit from remote master  branch");
 	}
 
 }
